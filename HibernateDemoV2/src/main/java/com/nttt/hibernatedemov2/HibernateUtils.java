f3b2d75b9d049785b379a2d24497555c9deb8641
Teaching2504 <tuyettrinhnguyenthi25042005@gmail.com>
2026-03-21 17:59:23 +0700

    Done Category

---
+++ HibernateDemoV2/src/main/java/com/nttt/hibernatedemov2/HibernateUtils.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.nttt.hibernatedemov2;
+
+import com.nttt.pojo.Category;
+import java.util.Properties;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
+
+
+/**
+ *
+ * @author MY PC
+ */
+
+public class HibernateUtils {
+    private static final SessionFactory FACTORY;
+
+    static {
+        Configuration conf = new Configuration();
+        Properties props = new Properties();
+        props.setProperty(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
+        props.setProperty(Environment.JAKARTA_JDBC_DRIVER, "com.mysql.cj.jdbc.Driver");
+        props.setProperty(Environment.JAKARTA_JDBC_URL, "jdbc:mysql://localhost/saledb");
+        props.setProperty(Environment.JAKARTA_JDBC_USER, "root");
+        props.setProperty(Environment.JAKARTA_JDBC_PASSWORD, "root");
+        props.setProperty(Environment.SHOW_SQL, "true");
+        
+        conf.setProperties(props);
+        
+        conf.addAnnotatedClass(Category.class);
+        
+        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                .applySettings(conf.getProperties()).build();
+
+        
+        FACTORY = conf.buildSessionFactory(serviceRegistry);
+    }
+    
+    
+    /**
+     * @return the FACTORY
+     */
+    public static SessionFactory getFACTORY() {
+        return FACTORY;
+    }
+    
+}
