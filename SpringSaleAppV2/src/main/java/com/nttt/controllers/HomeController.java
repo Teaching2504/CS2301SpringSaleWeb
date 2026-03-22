3d1ce7221b123bdcbbf8b82749e64e2621985c21
Teaching2504 <tuyettrinhnguyenthi25042005@gmail.com>
2026-03-22 16:29:27 +0700

    Created Web

---
+++ SpringSaleAppV2/src/main/java/com/nttt/controllers/HomeController.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.nttt.controllers;
+
+import com.nttt.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author admin
+ */
+@Controller
+public class HomeController {
+    @Autowired
+    private CategoryService cateService;
+    
+    @RequestMapping("/")
+    public String index(Model model){
+        model.addAttribute("categories",this.cateService.getCates());
+        
+        return "index";
+    }
+}
---
+++ SpringSaleAppV2/src/main/java/com/nttt/repositories/impl/CategoryRepositoryImpl.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.nttt.repositories.impl;
+
+import com.nttt.pojo.Category;
+import com.nttt.repositories.CategoryRepository;
+import jakarta.persistence.Query;
+import java.util.List;
+import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author MY PC
+ */
+@Repository
+public class CategoryRepositoryImpl implements CategoryRepository{
+    @Autowired
+    private LocalSessionFactoryBean factory;
+    
+    @Override
+    public List<Category> getCate(){
+        Session s = this.factory.getObject().getCurrentSession();
+        Query q = s.createQuery("FROM Category", Category.class);
+        return q.getResultList();
+           
+    }
+}
